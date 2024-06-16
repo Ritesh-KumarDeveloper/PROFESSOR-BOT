847824beb6e90746313bd2c03f40d57da6db1dbc
Ritesh-KumarDeveloper <140222517+Ritesh-KumarDeveloper@users.noreply.github.com>
2024-06-16 12:58:08 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -28,7 +28,7 @@ I Cᴀɴ Pʀᴏᴠɪᴅᴇ Mᴏᴠɪᴇ Fᴏʀ Yᴏᴜ Jᴜsᴛ Aᴅᴅ Mᴇ Iɴ
 ➪ /batch › Tᴏ Cʀᴇᴀᴛᴇ Lɪɴᴋ Fᴏʀ Mᴜʟᴛɪᴘʟᴇ Mᴇᴅɪᴀ
 
 <b>⪼ EG:</b>
-</code>/batch https://t.me/mkn_bots_updates/1 https://t.me/+i090FPsrsck0MzNl/10</code>"""
+</code>/batch https://t.me/mkn_bots_updates/1 https://t.me/mkn_bots_updates</code>"""
   
     FILTER_TXT = "Sᴇʟᴇᴄᴛ Wʜɪᴄʜ Oɴᴇ Yᴏᴜ Wᴀɴᴛ...✨"
     
